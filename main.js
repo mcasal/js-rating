f61b55683b868a2344095e56d54887c878baff1e
Marta Casal Tàpia <>
2020-11-02 12:57:28 +0100

    all done

--- main.js
+++ main.js
@@ -3,28 +3,37 @@ const smiley2 = document.getElementById('dos');
 const smiley3 = document.getElementById('tres');
 const smiley4 = document.getElementById('quatre');
 const smiley5 = document.getElementById('cinc');
-// const canvas = document.getElementById('canvas');
-// const children = document.getElementById('canvas').children[0];
+const canvas = document.getElementById('canvas');
 
-// recorda que està row-reverse
-console.log(smiley3.nextElementSibling);
+const arrSmiley = [smiley1, smiley2, smiley3, smiley4, smiley5];
+arrSmiley.reverse();
 
-smiley1.addEventListener("click", function(){
-    smiley1.classList.toggle('checked');
-});
+smiley1.addEventListener("click", checkingSmiley);
 
-smiley2.addEventListener("click", function(){
-    smiley2.classList.toggle('checked');
-});
+smiley2.addEventListener("click", checkingSmiley);
 
-smiley3.addEventListener("click", function(){
-    smiley3.classList.toggle('checked');
-});
+smiley3.addEventListener("click", checkingSmiley);
 
-smiley4.addEventListener("click", function(){
-    smiley4.classList.toggle('checked');
-});
+smiley4.addEventListener("click", checkingSmiley);
 
-smiley5.addEventListener("click", function(){
-    smiley5.classList.toggle('checked');
+smiley5.addEventListener("click", checkingSmiley);
+
+
+// cada cop que es clica a un element, crida a la funció checkingSmiley. Aquesta funció primer elimina la class 'checked' de tots els elements de la array, després mira quin és l'element seleccionat i busca la coincidència a l'array. Un cop trobada, crea una nova array amb els elements que han de ser colorejats i afegeix la class 'checked' a la nova array.
+function checkingSmiley(element){
+    arrSmiley.forEach(smiley => smiley.classList.remove('checked'));
+    for (let i = 0; i < arrSmiley.length; i++) {
+        if (element.currentTarget === arrSmiley[i]) {
+            let checkSmiley = arrSmiley.slice(0, i+1);
+            checkSmiley.forEach(el => el.classList.toggle('checked'));
+        }
+    }
+}
+
+// cada cop que es clica al div i sempre i quant no siguin els smiley, esborrarà la class 'checked'
+canvas.addEventListener("click", (e) => {
+    let outsideSmiley = canvas.contains(e.target.querySelector('i')); // això retorna true si clica al canvas. Si es clica al smiley retorna false
+    if (outsideSmiley) {
+        arrSmiley.forEach(smiley => smiley.classList.remove('checked'));
+    }
 });
